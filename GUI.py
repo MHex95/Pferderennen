3a56244dbaaff170ab66aee896520029baa0a318
Martin Heuer <martin.heuer@yahoo.de>
2021-05-21 19:38:25 +0200

    Button RennenFrame gibt das gewählte Pferd aus

--- GUI.py
+++ GUI.py
@@ -5,6 +5,12 @@ import tkinter as tk
 import tkinter.font as tkFont
 
 
+def abfrage_wette(pferd_auswahl, wettbetrag):
+    tk.messagebox.askokcancel(title="")
+
+
+
+
 class Mainmenue(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
@@ -34,14 +40,14 @@ class Mainmenue(tk.Frame):
         raceframe.label = tk.Label(text="Wette platzieren", font=self.title_font, width=20)
         raceframe.label.grid(row=0, column=0)
 
-        Teilnehmer = [("Pferd 1", 1), ("Pferd2", 2), ("Pferd3", 3)]
+        Teilnehmer = [("Pferd 1", 0), ("Pferd 2", 1), ("Pferd 3", 2)]
 
-        int_auswahl = tk.IntVar()
+        pferd_auswahl = tk.IntVar()
         c = 1
 
         for text, auswahl in Teilnehmer:
             radiobutton = tk.Radiobutton(text=text)
-            radiobutton["variable"] = int_auswahl
+            radiobutton["variable"] = pferd_auswahl
             radiobutton["value"] = auswahl
             radiobutton.grid(row=c, column=0)
             c += 1
@@ -49,14 +55,6 @@ class Mainmenue(tk.Frame):
         wettbetrag_entry = tk.Entry()
         wettbetrag_entry.grid(row=c+1, column=0)
 
-        wettbestätigung = tk.Button(text="Wette platzieren", command=lambda: print(int_auswahl.get(),
-                                                                                   wettbetrag_entry.get()))
+        wettbestätigung = tk.Button(text="Wette platzieren",
+                                    command=lambda: print(Teilnehmer[pferd_auswahl.get()][0]))
         wettbestätigung.grid(row=c+2, column=0)
-
-
-'''class RennenFrame(tk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-        self.label = tk.Label(text="Das Rennen hat begonnen!!!!!", width=20)
-        self.label.pack()'''
